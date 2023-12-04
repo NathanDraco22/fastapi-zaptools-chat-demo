312173a220e816ecf9673153b258384ea75ff183
NathanDraco22 <nathandraco22@gmail.com>
2023-12-03 22:10:49 -0600

    init

---
+++ main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+from zaptools.connectors import FastApiConnector
+from zaptools.tools import EventRegister, EventContext
+from zaptools.room import Room
+
+
+app: FastAPI= FastAPI(title= "Chat Server")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+reg: EventRegister= EventRegister()
+room: Room = Room("chats")
+
+@reg.on_event("connected")
+async def on_connected(ctx: EventContext):
+    print("client connected")
+
+@reg.on_event("join-room")
+async def on_join_to_room(ctx: EventContext):
+    room.add(ctx.connection)
+    room.send("user-joined", payload=ctx.payload, exclude=ctx.connection)
+
+@reg.on_event("disconnected")
+async def on_disconnected(ctx: EventContext):
+    room.remove(ctx.connection)
+    room.send("user-left", {}, exclude= ctx.connection)
+    print("client disconnected")
+
+@reg.on_event("confirm")
+async def on_confirm(ctx: EventContext):
+    room.send("new", "new-connected", exclude= ctx.connection)
+    print(ctx.payload)
+
+@reg.on_event("send")
+async def on_send(ctx: EventContext):
+    print(ctx.payload)
+    room.send("new-message", ctx.payload, exclude=ctx.connection)
+    print(ctx.payload)
+
+
+
+@app.websocket("/ws")
+async def websocket_controller(ws: WebSocket ):
+   processor = await FastApiConnector.plug(register= reg, websocket= ws)
+   await processor.start_event_stream()
